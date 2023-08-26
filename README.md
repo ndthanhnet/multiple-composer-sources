f60cf9d17c3d8376861dbebce830cfb6afb33974
Thanh, Nguyen Duy <nd.thanh@outlook.com>
2023-08-26 16:55:48 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Multiple Composer Sources
+This utility helps you to install packages from same domain using multiple usernames and password
+
+## User cases
+- Using multiple Magento 2 marketplace accounts with composer in the same installation
---
+++ subrepo.sh
@@ -0,0 +1,87 @@
+# !/bin/bash
+# manage sub repositories with givent key and packages
+DIR="${BASH_SOURCE%/*}"
+COMPOSER="$(which composer)"
+GREP="$(which grep)"
+
+# include parts for environment initialization
+function execute() {
+    SUBDIR=$1
+    shift
+    PUBLIC_KEY=$2
+    shift
+    PRIVATE_KEY=$3
+    shift
+    PACKAGES=$@
+
+    # try to locate composer again
+    if [ -z "$COMPOSER" ]; then 
+        if [ -f "./composer.phar" ]; then
+            COMPOSER="./composer.phar"
+        else
+            echo "composer not found, please install composer or place composer.phar in Magento webroot directory";
+            exit;
+        fi  
+    fi
+
+    # crate sub dir if not exist
+    if [ ! -d "$DIR/$SUBDIR" ]; then
+        mkdir -p $DIR/$SUBDIR
+    fi
+
+    cd $DIR/$SUBDIR
+
+    # init composer.json
+    if [ -f "./composer.json" ]; then
+        echo "$DIR/$SUBDIR/composer.json exists"
+    else
+        echo "generating composer.json"
+        $COMPOSER init --no-interaction --name="$SUBDIR/repo" --description="$SUBDIR repository with different key" --stability="dev" --repository="https://repo.magento.com/"
+    fi
+
+    # init auth.json
+    if [ -f "./auth.json" ]; then
+        echo "$DIR/$SUBDIR/auth.json exists"
+    else
+        echo $PWD
+        echo "generating auth.json"
+        $COMPOSER config --no-interaction http-basic.repo.magento.com d8cf8882e7e04b12631a2c16f43c02b0 3adc070b740237aefc90c61ff7c407e0
+    fi
+
+    # loop through $PACKAGES and install packages that were not installed
+    for PACKAGE in $PACKAGES
+    do
+        if $GREP -q $PACKAGE "./composer.json"; then
+            echo "$PACKAGE exists"
+        else
+            $COMPOSER require $PACKAGE --no-interaction --no-update
+        fi
+    done
+
+    # check last composer update time and update if older than 30 days
+    TO_UPDATE=0
+    if [ -d "./vendor" ]; then
+        LAST_UPDATE=$(stat -c %Y "./composer.lock")
+        CURRENT_TIME=$(date +%s)
+        DIFFERENCE=$((CURRENT_TIME-LAST_UPDATE))
+        if [ $DIFFERENCE -gt 2592001 ]; then
+            TO_UPDATE=1
+        fi
+    else
+        TO_UPDATE=1
+    fi
+
+    if [ $TO_UPDATE -eq 1 ]; then
+        echo "modules not installed or oudated, running composer update..."
+        $COMPOSER update --no-interaction
+    fi
+
+    cd ../..
+
+    # configure local src
+    #cd ../..
+    #$COMPOSER config repositories.local-src path "local-src/$SUBDIR/vendor/*/*"
+    #$COMPOSER config scripts.pre-install-cmd "bash local-src/subrepo.sh jajuma d8cf8882e7e04b12631a2c16f43c02b0 3adc070b740237aefc90c61ff7c407e0 jajuma/bfcache jajuma/pagepreload jajuma/assetpreload"
+}
+
+execute $@
